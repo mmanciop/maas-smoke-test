4e4ad727cadf683b0d4453c1d71b91fe8a395d68
Michele Mancioppi <michele.mancioppi@canonical.com>
2021-12-16 11:55:51 +0100

    Improve docu and rollout steps

--- README.md
+++ README.md
@@ -4,25 +4,50 @@ This repository provides setup instructions for running functional smoke tests a
 
 ## Setup
 
-1. On a host with the NRPE subordinate charm installed, copy recursively the content of the [`nrpe_host`](./nrpe_host)folder of this repository from the root. TODO: Check if we can do it with `juju scp`.
-
-2. At this point you already can trigger the manual execution of the check with:
+1. On a host with the NRPE subordinate charm installed, install `pip3` and the `python-libmaas` library:
 
     ```sh
-    juju exec --unit <nrpe_unit_name> run-nrpe-check name=check_maas_provisioning --wait
+    juju exec --unit <nrpe_unit_name> apt update \; apt install python3-pip \; pip3 install python-libmaas
+    ```
+
+1. Enter the MAAS API URL in `nrpe_host/etc/nagios/nrpe.d/check_maas_provisioning.cfg`
+
+1. On a host with the NRPE subordinate charm installed, copy recursively the content of the [`nrpe_host`](./nrpe_host) folder of this repository from the root:
+
+    ```sh
+    juju scp nrpe_host/etc/nagios/nrpe.d/* <nrpe_unit_name>:/etc/nagios/nrpe.d/
+    juju scp nrpe_host/usr/local/lib/nagios/plugins/* <nrpe_unit_name>:nrpe_host/usr/local/lib/nagios/plugins/
+    ```
+
+1. Ensure the NRPE check can be executed:
+
+    ```sh
+    juju exec --unit <nrpe_unit_name> chmod +x /usr/local/lib/nagios/plugins/check_maas_provisioning.sh
+    ```
+
+1. Create a file containing an MAAS admin API key to be used by the check:
+
+    ```sh
+    juju exec --unit <nrpe_unit_name> -- echo -n "${MAAS_API_KEY}" \> /var/lib/juju/agents/unit-nrpe-0/charm/maas_api_key
+    ```
+
+1. At this point you already can trigger the manual execution of the check with:
+
+    ```sh
+    juju run-action <nrpe_unit_name> run-nrpe-check name=check_maas_provisioning --wait
     ```
 
     Replace in the command above `<nrpe_unit_name>` with the actual unit name of the subordinate `NRPE`](https://charmhub.io/nrpe) running on the host on which you have installed the files of this repository.
 
-3. Customize the `nagios_host/etc/nagios3/conf.d/maas-liveness-check.cfg` to add the right hosts or hostgroups into the `maas_hosts` host group (see the [Nagios3 Documentation](https://assets.nagios.com/downloads/nagioscore/docs/nagioscore/3/en/objectdefinitions.html#hostgroup) for reference.)
+1. Customize the `nagios_host/etc/nagios3/conf.d/maas-liveness-check.cfg` to add the right hosts or hostgroups into the `maas_hosts` host group (see the [Nagios3 Documentation](https://assets.nagios.com/downloads/nagioscore/docs/nagioscore/3/en/objectdefinitions.html#hostgroup) for reference.)
 
-4. Upload the new configuration to Nagios:
+1. Upload the new configuration to Nagios:
 
     ```sh
     juju config <nagios_app_name> extraconfig=@nagios_host/etc/nagios3/conf.d/maas-liveness-check.cfg
     ```
 
-5. Confirm that the Nagios configuration has reload correctly looking in `juju debug-log`. You can also verify the validity of Nagios config files via Juju exec:
+6. Confirm that the Nagios configuration has reload correctly looking in `juju debug-log`. You can also verify the validity of Nagios config files via Juju exec:
 
     ```sh
     juju exec --app <nagios_app_name> -- /usr/sbin/nagios3 -v /etc/nagios3/nagios.cfg
--- nrpe_host/usr/local/lib/nagios/plugins/check_maas_provisioning.sh
+++ nrpe_host/usr/local/lib/nagios/plugins/check_maas_provisioning.sh
@@ -1,3 +1,114 @@
-#!/bin/bash
+#!/usr/bin/env python3
 
-echo "IT'S ALIVE"
+""" Copyright (C) 2021  Canonical
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program - see the LICENSE file.  If not, see <https://www.gnu.org/licenses/>.
+ """
+
+import maas.client
+import argparse
+import os
+import signal
+
+from pathlib import Path
+
+MAAS_API_KEY_ENV_VAR_NAME = "MAAS_API_KEY"
+
+
+def get_maas_api_key():
+    maas_api_key_env = os.getenv(MAAS_API_KEY_ENV_VAR_NAME)
+
+    if maas_api_key_env:
+        return maas_api_key_env
+
+    maas_api_key_file_path = Path("./maas_api_key").absolute()
+
+    if not maas_api_key_file_path.is_file():
+        raise Exception(
+            "Neither the '{}' environment variable is set nor the '{}' MAAS API key file exists".format(
+                MAAS_API_KEY_ENV_VAR_NAME, maas_api_key_file_path
+            )
+        )
+
+    with open(maas_api_key_file_path) as file:
+        return file.read()
+
+
+def timeout_handler(signum, frame):
+    raise Exception("Timed out")
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Test a MAAS installation can refresh all LXD VM hosts. "
+        + "Returns 0 if all ok or 1 if any of them fail. API key must be stored in env variable 'MAAS_API_KEY', or in a 'maas_api_key' file in the working directory from which this program is launched."
+        + "Assumes MAAS runs on standard port 5240 and is http.",
+        epilog=" Example usage: python3 maas-lxd-ping.py -t http://192.168.200.16:5240/MAAS/ -u admin",
+    )
+    parser.add_argument(
+        "-t",
+        "--target",
+        help="Target MAAS API URL",
+        action="store",
+        default="http://localhost:5240/MAAS/",
+    )
+    parser.add_argument(
+        "-u",
+        "--user-name",
+        help="User name for MAAS host. Should be an admin.",
+        action="store",
+        default="admin",
+    )
+    args = parser.parse_args()
+
+    maas_api_key = None
+    try:
+        maas_api_key = get_maas_api_key().strip()
+    except Exception as e:
+        print(
+            "Couldn't retrieve the MAAS API key."
+        )
+        print(f"Exception: {e}")
+        exit(1)
+
+    client = None
+    try:
+        client = maas.client.connect(f"{args.target}", apikey=maas_api_key)
+    except Exception as e:
+        print(
+            "Couldn't log in. Invalid credentials or server not available."
+        )
+        print(f"Exception: {e}")
+        exit(1)
+
+    myself = client.users.whoami()
+    assert myself.is_admin, "%s is not an admin" % myself.username
+
+    signal.signal(signal.SIGALRM, timeout_handler)
+
+    pods = client.pods.list()
+    for p in pods:
+        pod = client.pods.get(p.id)
+        try:
+            signal.alarm(15)
+            x = pod.refresh()
+        except Exception as e:
+            print(f"Could not refresh pod with id: {p.id}.\nException: {e}")
+            exit(1)
+        signal.alarm(0)
+    exit(0)
+
+
+if __name__ == "__main__":
+    main()
